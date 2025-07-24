c4af30c3cf09a454a5684985520e9f421449dbce
vinay223344 <v647414@gmail.com>
2025-07-24 10:54:37 +0530

    Uploading Class1.cs via API

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Class1
+    {
+        static void Main() 
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Username");
+                String username = Console.ReadLine();
+                Console.WriteLine("Password");
+                String password = Console.ReadLine();
+                if (username == "184" && password == "hello")
+                {
+                    Console.WriteLine("Login sucessful");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid Mail or Password");
+                    Console.WriteLine("Please Enter correct details");
+                }
+            }
+        }
+    }
+}
